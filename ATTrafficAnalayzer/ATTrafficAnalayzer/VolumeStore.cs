8c195c765213f49ff05eaf4d6f0b55b8d8208ed6
Michael Little <bio595@gmail.com>
2013-03-25 13:01:53 +1300

    Added class to represent a Volume Store

---
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/VolumeStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ATTrafficAnalayzer
+{
+    public class VolumeStore
+    {
+        private byte[] _file;
+
+        public VolumeStore(String filename)
+        {
+            _file = File.ReadAllBytes(filename);
+        }
+    }
+}
