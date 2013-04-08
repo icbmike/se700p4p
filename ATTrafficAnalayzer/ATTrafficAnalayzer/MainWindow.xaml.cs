44e245aea5d912d1475d4944d977b4beb174301b
Michael Little <bio595@gmail.com>
2013-04-08 18:58:28 +1200

    Changed the VolumeStore class to be a Singleton and started writing the method that loads VS files.

--- ATTrafficAnalayzer/ATTrafficAnalayzer/MainWindow.xaml.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace ATTrafficAnalayzer
 {
@@ -42,7 +43,24 @@ namespace ATTrafficAnalayzer
 
         private void MenuItemImportClick(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("SICK");
+            // Configure open file dialog box 
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.FileName = ""; // Default file name
+            dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); //The initial directory
+            dlg.DefaultExt = ".VS"; // Default file extension 
+            dlg.Filter = "Volume Store Files (.VS)|*.VS"; // Filter files by extension 
+
+            // Show open file dialog box 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                VolumeStoreSingleton.getInstance().readFile(filename);
+
+            }
         }
     }
 }
--- ATTrafficAnalayzer/ATTrafficAnalayzer/VolumeStore.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/VolumeStore.cs
@@ -6,13 +6,62 @@ using System.IO;
 
 namespace ATTrafficAnalayzer
 {
-    public class VolumeStore
+    public class VolumeStoreSingleton
     {
-        private byte[] _file;
+        private Dictionary<DateTime, List<List<int>>> _volumesDictionary;
+        private static VolumeStoreSingleton instance;
 
-        public VolumeStore(String filename)
+        private VolumeStoreSingleton()
         {
-            _file = File.ReadAllBytes(filename);
+            _volumesDictionary = new Dictionary<DateTime, List<List<int>>>();
+        }
+
+        public static VolumeStoreSingleton getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new VolumeStoreSingleton();
+            }
+            return instance;
+        }
+
+        public void readFile(string filename)
+        {
+            //Load the file into memory
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            int sizeInBytes = (int)fs.Length;
+            byte[] byteArray = new byte[sizeInBytes];
+            fs.Read(byteArray, 0, sizeInBytes);
+
+            //Now decrypt it
+            int index = 0;
+            while (index < sizeInBytes) //seek through the byte array untill we reach the end
+            {
+                int recordSize = byteArray[index] + byteArray[index+1] * 256; //The record size is stored in two bytes, little endian
+                Console.WriteLine(recordSize);
+                index += 2;
+
+                byte[] record;
+                if (recordSize % 2 == 0) //Records with odd record length have a trailing null byte.
+                {
+                    record = byteArray.Skip(index).Take(recordSize).ToArray();
+                    index += recordSize;
+                }
+                else
+                {
+                    record = byteArray.Skip(index).Take(recordSize + 1).ToArray();
+                    index += recordSize + 1;
+                }
+
+                //TODO: Do stuff with the record
+
+            }
+
+        }
+
+        private static bool getBit(byte b, int pos)
+        {
+            return (b & (1 << pos - 1)) != 0;
         }
     }
 }
