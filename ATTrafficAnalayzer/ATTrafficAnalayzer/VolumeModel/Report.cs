00dbe7cab8cd34d20a1b101390f0a502bbe1a248
Andrew Luey <lueylueyluey@gmail.com>
2013-06-14 20:22:45 +1200

    Created models

---
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/VolumeModel/Report.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ATTrafficAnalayzer.VolumeModel
+{
+    class Report
+    {
+        private String _name;
+        private List<AbstractApproach> _approaches = new List<AbstractApproach>();
+
+        public String Name {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public List<AbstractApproach> Approaches
+        {
+            get { return _approaches; }
+            set { _approaches = value; }
+        }
+
+        public Report(String name)
+        {
+            _name = name;
+        }
+
+        public Report(String name, List<AbstractApproach> approaches)
+        {
+            _name = name;
+            _approaches = approaches;
+        }
+    }
+}
---
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/VolumeModel/Route.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ATTrafficAnalayzer.VolumeModel
+{
+    class Route : AbstractApproach
+    {
+        private List<Intersection> _intersections = new List<Intersection>();
+
+        public List<Intersection> Intersections
+        {
+            get { return _intersections; }
+            set { _intersections = value; }
+        }
+
+        public void addIntersection(Intersection intersection)
+        {
+            _intersections.Add(intersection);
+        }
+
+        public void removeIntersection(Intersection Intersection)
+        {
+            _intersections.Remove(Intersection);
+        }
+
+        public void clearIntersections()
+        {
+            _intersections = new List<Intersection>();
+        }
+
+        public int getVolume()
+        {
+            //TODO for each intersection query the database and return the sum
+            return 0;
+        }
+    }
+}
