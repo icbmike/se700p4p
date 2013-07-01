f781a3508ad0e7127ceb4f04fc1b87d2cb812b67
Mike Little <bio595@gmail.com>
2013-07-01 12:08:20 +1200

    began porting table to dbhelper

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/VSTable.xaml.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/VSTable.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Data;
+using ATTrafficAnalayzer.VolumeModel;
 
 namespace ATTrafficAnalayzer.Views
 {
@@ -11,46 +12,51 @@ namespace ATTrafficAnalayzer.Views
     /// <summary>
     /// Interaction logic for VSSCreen.xaml
     /// </summary>
-    public partial class VsTable : UserControl
+    public partial class VsTable
     {
+        private readonly SettingsTray _settings;
+        private readonly string _configName;
 
         private int _interval;
         private DateTime _startDate;
         private DateTime _endDate;
+        private VolumeDbHelper _volumeDbHelper;
+        private ReportConfiguration _configuration;
 
 
-        /*
-        public VsTable(SettingsTray settings, string configName) : base(settings, configName)
+        public VsTable(SettingsTray settings, string configName)
         {
-         
-            _
+            _settings = settings;
+            _volumeDbHelper = VolumeDbHelper.GetDbHelper();
+
+            //Retrieve the config for the supplied name
+            _configuration = _volumeDbHelper.GetConfiguration(_configName);
+
             InitializeComponent();
 
-            var dateLabel = new Label {Content = string.Format("Day: {0} Time range: {1}", "", ""), Margin = new Thickness(10)};
-            putStuffHere.Children.Add(dateLabel);
+            var dateLabel = new Label { Content = string.Format("Day: {0} Time range: {1}", "", ""), Margin = new Thickness(10) };
+            ContainerStackPanel.Children.Add(dateLabel);
 
-            var dg = new DataGrid
+            foreach (var approach in _configuration.Approaches)
+            {
+                var dg = new DataGrid
                 {
-                    ItemsSource = GenerateVsTable().AsDataView(),
+                    ItemsSource = GenerateVsTable(approach).AsDataView(),
                     Margin = new Thickness(10),
                     Width = Double.NaN,
                     Height = 280,
                     ColumnWidth = new DataGridLength(1, DataGridLengthUnitType.Star),
                     IsReadOnly = true
                 };
-            putStuffHere.Children.Add(dg);
+                ContainerStackPanel.Children.Add(dg);    
+            }           
 
             Logger.Info("constructed view", "VS table");
         }
 
-        public VsTable(SettingsTray volumeStore, object selectedItem)
+        public DataTable GenerateVsTable(Approach approach)
         {
             
-        }
-
-        public DataTable GenerateVsTable()
-        {
-           
             // Create a DataGrid
             var vsDataTable = new DataTable();
 
@@ -61,19 +67,16 @@ namespace ATTrafficAnalayzer.Views
             }
 
             // List dates
-            var ds = new List<DateTime>();
-            foreach (var date in _volumeStore.DateTimeRecords)
+            var dates = new List<DateTime>();
+            for (var date = _settings.StartDate; date < _settings.EndDate; date = date.AddMinutes(_settings.Interval))
             {
-                ds.Add(date.DateTime);
+                dates.Add(date);
             }
-            var dates = ds.ToArray();
-
-            // List intersections
-            var intersection = _volumeStore.GetIntersections().ToList()[0]; // Use the first intersection for the time being
-
-            // List detectors
-            var detector = _volumeStore.GetDetectorsAtIntersection(intersection)[0]; // Use the first detector for the time being
-
+            foreach (var detector in approach.Detectors)
+            {
+                _volumeDbHelper.GetVolumes(_configuration.Intersection, detector, _settings.StartDate, _settings.EndDate);    
+            }
+            
             // Get volume store data
             for (var i = 0; i < 12; i++)
             {
@@ -88,7 +91,6 @@ namespace ATTrafficAnalayzer.Views
             return vsDataTable;
         }
     
-         * 
-         */
+
     }
 }
