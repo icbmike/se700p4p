b804904de63e685ec9af0f7d000364dac222d1d1
Mike Little <bio595@gmail.com>
2013-07-26 18:25:37 +1200

    Made welcomescreen control display imported dates and hooked up button to do import. Fixes #89

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/WelcomeScreen.xaml.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/WelcomeScreen.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using ATTrafficAnalayzer.Models;
 
@@ -9,26 +10,33 @@ namespace ATTrafficAnalayzer.Views.Screens
     /// </summary>
     public partial class WelcomeScreen : UserControl
     {
-        public WelcomeScreen()
+
+        #region events
+
+        public delegate void ImportRequestEventHandler(object sender, RoutedEventArgs e);
+
+        public event ImportRequestEventHandler ImportRequested;
+
+        #endregion
+
+
+        public WelcomeScreen(ImportRequestEventHandler handler)
         {
             InitializeComponent();
-
+            ImportRequested += handler;
             DbHelper helper = DbHelper.GetDbHelper();
 
             var importedDates = helper.GetImportedDates();
-            recentStandardReportsListBox.ItemsSource = importedDates;
+            ImportedDatesList.ItemsSource = importedDates;
 
-            var recentSpecialReports = new List<string>
-                {
-                    "Recent Special Report 1",
-                    "Recent Special Report 2",
-                    "Recent Special Report 3",
-                    "Recent Special Report 4",
-                    "Recent Special Report 5"
-                };
-            recentSpecialReportsListBox.ItemsSource = recentSpecialReports;
 
             Logger.Info("constructed view", "homescreen");
         }
+
+        private void ImportButtonClick(object sender, RoutedEventArgs e)
+        {
+            ImportRequested(this, e);
+        }
+
     }
 }
