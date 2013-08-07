e5b1cbaa813abd74b8984480d9a19f3a7a19f7df
Andrew Luey <lueylueyluey@gmail.com>
2013-08-08 11:35:11 +1200

    Created event handlers

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/Summary.xaml.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/Summary.xaml.cs
@@ -1,15 +1,62 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using ATTrafficAnalayzer.Models;
+using ATTrafficAnalayzer.Models.Configuration;
+using ATTrafficAnalayzer.Models.Settings;
+using ATTrafficAnalayzer.Views.Controls;
 
 namespace ATTrafficAnalayzer.Views.Screens
 {
     /// <summary>
     /// Interaction logic for Summary.xaml
     /// </summary>
-    public partial class Summary : UserControl
+    public partial class Summary
     {
-        public Summary()
+        private readonly DbHelper _dbHelper;
+        private DateTime _endDate;
+        private DateTime _startDate;
+        private Report _configuration;
+
+        public Summary(SettingsTray settings, string configName)
         {
+            _startDate = settings.StartDate;
+            _endDate = settings.EndDate;
+            _dbHelper = DbHelper.GetDbHelper();
+            _configuration = _dbHelper.GetConfiguration(configName);
+
             InitializeComponent();
+
+            FillSummary(); 
+        }
+
+        internal void DateRangeChangedHandler(object sender, Toolbar.DateRangeChangedEventHandlerArgs args)
+        {
+            if (!args.startDate.Equals(_startDate) || !args.endDate.Equals(_endDate))
+            {
+                _startDate = args.startDate;
+                _endDate = args.endDate;
+            }
+            FillSummary();
+        }
+
+        public void ReportChangedHandler(object sender, ReportBrowser.SelectedReporChangeEventHandlerArgs args)
+        {
+            _configuration = _dbHelper.GetConfiguration(args.ReportName);
+            FillSummary();
+        }
+
+        internal void ReportChangedHandler(object sender, Toolbar.DateRangeChangedEventHandlerArgs args)
+        {
+            if (!args.startDate.Equals(_startDate) || !args.endDate.Equals(_endDate))
+            {
+                _startDate = args.startDate;
+                _endDate = args.endDate;
+            }
+            FillSummary();
+        }
+
+        private static void FillSummary()
+        {
+            
         }
     }
 }
