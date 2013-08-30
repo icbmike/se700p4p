f9dfa1b3f03090888868bca1872dff3335688048
Mike Little <bio595@gmail.com>
2013-08-31 01:01:06 +1200

    wrote comments for IView and IConfigScreen

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/Iview.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/Iview.cs
@@ -2,8 +2,16 @@
 
 namespace ATTrafficAnalayzer.Views.Screens
 {
+    /// <summary>
+    /// Delegate that lets subscribers know when volume counts and date counts dont match.
+    /// </summary>
+    /// <param name="sender">The IView that tried to display volume data for some date range</param>
     public delegate void VolumeAndDateCountsDontMatchHandler(IView sender);
 
+    /// <summary>
+    /// Interface that lets implementors respond to changes in the ReportBrowser and Toolbar components. 
+    /// Also lets other componenets know when volume and date counts don't match.
+    /// </summary>
     public interface IView
     {
         void DateRangeChangedHandler(object sender, Toolbar.DateRangeChangedEventHandlerArgs args);
