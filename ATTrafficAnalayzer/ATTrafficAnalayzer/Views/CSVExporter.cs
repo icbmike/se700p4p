b2bef5e16fb1c455bdfae0d66479bb0133817e94
Andrew Luey <lueylueyluey@gmail.com>
2013-08-31 20:52:37 +1200

    Formatted csv output and now includes all summaries

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/CSVExporter.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/CSVExporter.cs
@@ -100,19 +100,29 @@ namespace ATTrafficAnalayzer.Views
 
         public void ExportSummary()
         {
-            var amSummary = _dtHelper.GetSummaryDataTable(new ATTrafficAnalayzer.Models.Configuration.DataTableHelper.AmPeakCalculator(_settings.SummaryAmPeak), _settings.StartDate, _settings.EndDate, _dbHelper.GetSummaryConfig(_configName));
+            Dictionary<string, DataTable> summaries = new Dictionary<string, DataTable>();
+            summaries.Add("AM Peak Hour Volumes", _dtHelper.GetSummaryDataTable(new ATTrafficAnalayzer.Models.Configuration.DataTableHelper.AmPeakCalculator(_settings.SummaryAmPeak), _settings.StartDate, _settings.EndDate, _dbHelper.GetSummaryConfig(_configName)));
+            summaries.Add("PM Peak Hour Volumes", _dtHelper.GetSummaryDataTable(new ATTrafficAnalayzer.Models.Configuration.DataTableHelper.PmPeakCalculator(_settings.SummaryPmPeak), _settings.StartDate, _settings.EndDate, _dbHelper.GetSummaryConfig(_configName)));
+            summaries.Add("Total Traffic Volumes", _dtHelper.GetSummaryDataTable(new ATTrafficAnalayzer.Models.Configuration.DataTableHelper.SumCalculator(), _settings.StartDate, _settings.EndDate, _dbHelper.GetSummaryConfig(_configName)));
 
             var lines = new List<string>();
+            foreach (var summary in summaries)
+            {
+                lines.Add(summary.Key);
 
-            string[] columnNames = amSummary.Columns.Cast<DataColumn>().
-                                              Select(column => column.ColumnName).
-                                              ToArray();
-            var header = string.Join(",", columnNames);
-            lines.Add(header);
+                string[] columnNames = summary.Value.Columns.Cast<DataColumn>().
+                                                  Select(column => column.ColumnName).
+                                                  ToArray();
+                var header = string.Join(",", columnNames);
+                lines.Add(header);
 
-            var valueLines = amSummary.AsEnumerable()
-                .Select(row => string.Join(",", row.ItemArray));
-            lines.AddRange(valueLines);
+                var valueLines = summary.Value.AsEnumerable()
+                    .Select(row => string.Join(",", row.ItemArray));
+                valueLines = valueLines.Select(row => row.Remove(row.IndexOf(','), 1));
+                lines.AddRange(valueLines);
+
+                lines.Add("");
+            }
 
             File.WriteAllLines(_outputFilename, lines);
         }
