dbd13d2bfd141dfe2e076dfa95170afe4bfaa8c4
Mike Little <bio595@gmail.com>
2013-09-19 21:11:24 +1200

    moved enumtobooleanconverter

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Controls/EnumMatchToBooleanConverter.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Controls/EnumMatchToBooleanConverter.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 
-namespace ATTrafficAnalayzer
+namespace ATTrafficAnalayzer.Views.Controls
 {
     public class EnumMatchToBooleanConverter : IValueConverter
     {
