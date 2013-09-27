74534f36281bb592f8030a426c236e91184bd555
Mike Little <bio595@gmail.com>
2013-09-27 14:48:37 +1200

    wrote a bunch of coments for main window

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/MainWindow.xaml.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace ATTrafficAnalayzer.Views
         private int _amPeakIndex = 8;
         private int _pmPeakIndex = 4;
 
+        /// <summary>
+        /// Default constructor used by App
+        /// </summary>
         public MainWindow()
         {
             Logger.Clear();
@@ -28,6 +31,7 @@ namespace ATTrafficAnalayzer.Views
 
             InitializeComponent();
 
+            //Set the content screen to be a Home
             var homeScreen = new Home();
             homeScreen.ImportRequested += FileImportMenuItem_Click;
 
@@ -41,27 +45,43 @@ namespace ATTrafficAnalayzer.Views
 
         #region Screen Switching
 
+        /// <summary>
+        /// Removes event handlers from a screen, 
+        /// determines what kind of screen and then calls that specific RemoveHandlers method
+        /// </summary>
+        /// <param name="screen"></param>
         private void RemoveHandlers(object screen)
         {
             if (screen as IConfigScreen != null)
                 RemoveHandlers(screen as IConfigScreen);
             else if (screen as IView != null)
                 RemoveHandlers(screen as IView);
-            //            else
-            //                MessageBox.Show("Somethings has gone horribly wrong");
         }
+
+        /// <summary>
+        /// Removes handlers from an IView
+        /// </summary>
+        /// <param name="iView"></param>
         private void RemoveHandlers(IView iView)
         {
             ReportBrowser.ReportChanged -= iView.ReportChangedHandler;
             SettingsToolbar.DateRangeChanged -= iView.DateRangeChangedHandler;
             iView.VolumeDateCountsDontMatch -= OnVolumeDateCountsDontMatch;
         }
+        /// <summary>
+        /// Removes handlers from an IConfigScreen
+        /// </summary>
+        /// <param name="iConfigScreen"></param>
         private void RemoveHandlers(IConfigScreen iConfigScreen)
         {
             iConfigScreen.ConfigurationSaved -= ReportBrowser.ConfigurationSavedEventHandler;
             iConfigScreen.ConfigurationSaved -= IConfigScreen_ConfigurationSaved;
         }
 
+        /// <summary>
+        /// Chang the content screen to be the supplied UserControl
+        /// </summary>
+        /// <param name="screen"></param>
         private void ChangeScreen(UserControl screen)
         {
             var oldScreen = ScreenContentControl.Content;
@@ -72,19 +92,29 @@ namespace ATTrafficAnalayzer.Views
                 RemoveHandlers(oldScreen);
         }
 
+        /// <summary>
+        /// Mode changed handler
+        /// </summary>
+        /// <param name="sender">Toolbar</param>
+        /// <param name="args"></param>
         private void SettingsToolbar_OnModeChanged(object sender, Toolbar.ModeChangedEventHandlerArgs args)
         {
+
             var prevMode = _mode;
             _mode = args.Mode;
 
+            //Tell the Report browser to clear its selected item if the mode has changed
             if (!prevMode.Equals(_mode) && ReportBrowser.GetSelectedConfiguration() != null)
                 ReportBrowser.ClearSelectedConfig();
 
+            //Get the selected configuration
             var selectedConfiguration = ReportBrowser.GetSelectedConfiguration();
-            Console.WriteLine(selectedConfiguration);
+
+            //Detemine what mode we have changed to.
             switch (_mode)
             {
                 case Mode.Home:
+                    //Display the Home screen
                     ReportBrowser.Visibility = Visibility.Collapsed;
                     var homeScreen = new Home();
                     homeScreen.ImportRequested += FileImportMenuItem_Click;
@@ -93,9 +123,11 @@ namespace ATTrafficAnalayzer.Views
                     break;
 
                 case Mode.Report:
+                    //Display either a config screen, table or graph.
                     ReportBrowser.Visibility = Visibility.Visible;
                     if (ReportBrowser.GetSelectedConfiguration() == null)
                     {
+                        //Create new configuartion screen if we switched from a different mode
                         var reportConfigurationScreen = new ReportConfig();
                         reportConfigurationScreen.Popup.Visibility = Visibility.Visible;
                         reportConfigurationScreen.ConfigurationSaved += ReportBrowser.ConfigurationSavedEventHandler;
@@ -105,9 +137,10 @@ namespace ATTrafficAnalayzer.Views
                         reportConfigurationScreen.Visibility = Visibility.Visible;
                         ChangeScreen(reportConfigurationScreen);
                     }
+                    //We havent changed mode, have only changed view
                     else if (args.View.Equals(Toolbar.View.Graph))
                     {
-
+                        //Create and display a new Graph
                         var graphScreen = new ReportGraph(SettingsToolbar.SettingsTray, ReportBrowser.GetSelectedConfiguration());
                         SettingsToolbar.DateRangeChanged += graphScreen.DateRangeChangedHandler;
                         ReportBrowser.ReportChanged += graphScreen.ReportChangedHandler;
@@ -116,6 +149,7 @@ namespace ATTrafficAnalayzer.Views
                     }
                     else if (args.View.Equals(Toolbar.View.Table))
                     {
+                        //Create and display a new Table
                         var tableScreen = new ReportTable(SettingsToolbar.SettingsTray, ReportBrowser.GetSelectedConfiguration());
                         SettingsToolbar.DateRangeChanged += tableScreen.DateRangeChangedHandler;
                         ReportBrowser.ReportChanged += tableScreen.ReportChangedHandler;
@@ -128,6 +162,7 @@ namespace ATTrafficAnalayzer.Views
                     ReportBrowser.Visibility = Visibility.Visible;
                     if (ReportBrowser.GetSelectedConfiguration() == null)
                     {
+                        //If we are switching from a different mode, display a config screen
                         var summaryConfigScreen = new SummaryConfig { Popup = { Visibility = Visibility.Visible } };
                         summaryConfigScreen.ConfigurationSaved += ReportBrowser.ConfigurationSavedEventHandler;
                         summaryConfigScreen.ConfigurationSaved += IConfigScreen_ConfigurationSaved;
@@ -137,6 +172,7 @@ namespace ATTrafficAnalayzer.Views
                     }
                     else
                     {
+                        //Else display a new Table
                         var summaryScreen = new SummaryTable(SettingsToolbar.SettingsTray,
                         ReportBrowser.GetSelectedConfiguration());
                         SettingsToolbar.DateRangeChanged += summaryScreen.DateRangeChangedHandler;
@@ -146,6 +182,7 @@ namespace ATTrafficAnalayzer.Views
                     break;
 
                 case Mode.Faults:
+                    //Display suspected faults
                     ReportBrowser.Visibility = Visibility.Collapsed;
                     var faultsScreen = new Faults(SettingsToolbar.SettingsTray);
                     SettingsToolbar.DateRangeChanged += faultsScreen.DateRangeChangedHandler;
@@ -155,16 +192,25 @@ namespace ATTrafficAnalayzer.Views
             }
         }
 
+        /// <summary>
+        /// Handler for when the report browser says that the user wants to create or edit a configuration.
+        /// </summary>
+        /// <param name="sender">Report Browser</param>
+        /// <param name="args"></param>
         private void ReportBrowser_OnEditConfigurationEvent(object sender, ReportBrowser.EditConfigurationEventHandlerArgs args)
         {
+            //Creating a new config
             if (args.New)
             {
+                //Either a new Config or Summary
                 if (_mode.Equals(Mode.Report))
                 {
                     if (SettingsToolbar.StartDatePicker.SelectedDate != null && SettingsToolbar.EndDatePicker.SelectedDate != null)
                     {
+                        //Check if volumes exist for the selected range
                         if (DbHelper.GetDbHelper().VolumesExist((DateTime) SettingsToolbar.StartDatePicker.SelectedDate, (DateTime) SettingsToolbar.EndDatePicker.SelectedDate))
                         {
+                            //Create and display a new Report config screen
                             var reportConfigurationScreen = new ReportConfig();
                             reportConfigurationScreen.ConfigurationSaved += ReportBrowser.ConfigurationSavedEventHandler;
                             reportConfigurationScreen.ConfigurationSaved += IConfigScreen_ConfigurationSaved;
@@ -179,8 +225,10 @@ namespace ATTrafficAnalayzer.Views
                 }
                 else
                 {
+                    //Check volumes exist for specified date range
                     if (DbHelper.GetDbHelper().VolumesExistForMonth(SettingsToolbar.Month))
                     {
+                        //Create and display a new summary config screen
                         var summaryConfigScreen = new SummaryConfig();
                         summaryConfigScreen.ConfigurationSaved += ReportBrowser.ConfigurationSavedEventHandler;
                         summaryConfigScreen.ConfigurationSaved += IConfigScreen_ConfigurationSaved;
@@ -194,6 +242,7 @@ namespace ATTrafficAnalayzer.Views
                     }
                 }
             }
+            //Editing an existing config
             else
             {
                 //Open relevant config screen
@@ -216,14 +265,26 @@ namespace ATTrafficAnalayzer.Views
             }
         }
 
+        /// <summary>
+        /// Handler for when the selected report changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void ReportChangedHandler(object sender, ReportBrowser.SelectedReportChangeEventHandlerArgs args)
         {
+            //Run same code as when a configuration has been saved
             if (!args.SelectionCleared && args.ReportName != null)
                 IConfigScreen_ConfigurationSaved(this, new ConfigurationSavedEventArgs(args.ReportName));
         }
 
+        /// <summary>
+        /// Handler for when a configuration has been saved
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         void IConfigScreen_ConfigurationSaved(object sender, ConfigurationSavedEventArgs args)
         {
+            //Display the appropriate Table view
             if (_mode.Equals(Mode.Report))
             {
                 var reportTableScreen = new ReportTable(SettingsToolbar.SettingsTray, args.Name);
@@ -244,15 +305,28 @@ namespace ATTrafficAnalayzer.Views
 
         #region File Importing
 
+        /// <summary>
+        /// Delegate and event for when importing a file has completed
+        /// </summary>
+        /// <param name="sender"></param>
         public delegate void ImportCompletedHandler(object sender);
         public event ImportCompletedHandler ImportCompleted;
 
+        /// <summary>
+        /// Event handler for when the window finished loading.
+        /// Prompts the user to populate the volume source if it is empty
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             if (DbHelper.VolumesTableEmpty())
                 BulkImport();
         }
 
+        /// <summary>
+        /// Asks the user to repeatedly import files
+        /// </summary>
         private void BulkImport()
         {
             var messageBoxText = "There is currently no volume data in the database. Would you like to import this data now?";
@@ -266,12 +340,16 @@ namespace ATTrafficAnalayzer.Views
                 messageBoxText = "Would you like to import another file?";
 
                 if (result.Equals(MessageBoxResult.Yes))
+                    //Actually import files
                     ImportFile();
                 else
                     break;
             }
         }
 
+        /// <summary>
+        /// Method to select and import multiple volume files
+        /// </summary>
         private void ImportFile()
         {
             // Configure open file dialog box 
@@ -291,19 +369,23 @@ namespace ATTrafficAnalayzer.Views
             // Process open file dialog box results 
             if (result == true)
             {
+                //Use the progress dialog library.
                 var settings = new ProgressDialogSettings(true, false, false);
                 foreach (var filename in dlg.FileNames)
                 {
+                    //Default duplicate policy
                     var skipAllOrOne = DbHelper.DuplicatePolicy.Skip;
+
+                    //Execture the background task of importing a file
                     ProgressDialog.Execute(this, "Importing VS File: " + filename.Substring(filename.LastIndexOf("\\") + 1), (b, w) =>
                     {
                         // Open document 
                         skipAllOrOne = DbHelper.ImportFile(b, w, filename,
                                                            progress =>
                                                            ProgressDialog.ReportWithCancellationCheck(b, w, progress, "Reading File"),
-                                                           GetDuplicatePolicy);
+                                                           GetDuplicatePolicy); //Function to determine a duplicate policy
 
-
+                        //Worker completed handler, emit ImportCompleted event.
                         b.RunWorkerCompleted += (sender, args) => { if (ImportCompleted != null) ImportCompleted(this); };
 
                     }, settings);
@@ -312,13 +394,20 @@ namespace ATTrafficAnalayzer.Views
             }
         }
 
+        /// <summary>
+        /// Method to get a duplicate policy.
+        /// Will return either the duplicate policy in the settings or ask the user on each duplicate conflict.
+        /// </summary>
+        /// <returns>The selected duplicate policy.</returns>
         private DbHelper.DuplicatePolicy GetDuplicatePolicy()
         {
             DbHelper.DuplicatePolicy policy;
+            //If the setting is to ask...
             if (_defaultDupicatePolicy.Equals(DefaultDupicatePolicy.Ask))
             {
                 var waitForInput = true;
 
+                //Use a dispatcher to display a dialog to get a choice from the user, because were proably on a background thread.
                 policy = DbHelper.DuplicatePolicy.Continue;
                 Dispatcher.BeginInvoke(new Action(() =>
                     {
@@ -331,6 +420,7 @@ namespace ATTrafficAnalayzer.Views
                 {
                 }
             }
+            //Else use the setting
             else if (_defaultDupicatePolicy.Equals(DefaultDupicatePolicy.Skip))
             {
                 policy = DbHelper.DuplicatePolicy.Skip;
@@ -347,12 +437,23 @@ namespace ATTrafficAnalayzer.Views
 
         #region File Exporting
 
+        /// <summary>
+        /// DateRangeChanged handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void SettingsToolbar_DateRangeChanged(object sender, Toolbar.DateRangeChangedEventHandlerArgs args)
         {
             _amPeakIndex = args.AmPeakHour;
             _pmPeakIndex = args.PmPeakHour;
         }
 
+        /// <summary>
+        /// Handler for when the report browser asks to export a config
+        /// Creates a dialog to ask the user for a filename and then uses a CSVExporter to do the deed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void ReportBrowser_OnExportEvent(object sender, ReportBrowser.EditConfigurationEventHandlerArgs args)
         {
             var dlg = new SaveFileDialog
@@ -378,16 +479,30 @@ namespace ATTrafficAnalayzer.Views
 
         #region Menu Event Handlers
 
+        /// <summary>
+        /// Click handler for menu item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FileImportMenuItem_Click(object sender, RoutedEventArgs e)
         {
             ImportFile();
         }
 
+        /// <summary>
+        /// Click handler for menu item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FileExitMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-
+        /// <summary>
+        /// Click handler for menu item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void HelpAboutUsMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
             // Configure the message box to be displayed 
@@ -402,6 +517,10 @@ namespace ATTrafficAnalayzer.Views
 
         #endregion
 
+        /// <summary>
+        /// Event handler for when an IView syas that volume and date cpunts dont match
+        /// </summary>
+        /// <param name="sender"></param>
         private void OnVolumeDateCountsDontMatch(IView sender)
         {
             MessageBox.Show("You don't have volume data imported for the range you specified");
