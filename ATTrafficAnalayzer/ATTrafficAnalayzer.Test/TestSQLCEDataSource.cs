0ebd2b197ec2cdebe3c76781b149a2d7d9dd5ba5
Michael Little <bio595@gmail.com>
2013-12-07 21:51:38 +1300

    added 4 more tests

--- ATTrafficAnalayzer/ATTrafficAnalayzer.Test/TestSQLCEDataSource.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer.Test/TestSQLCEDataSource.cs
@@ -1,24 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using ATTrafficAnalayzer.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ATTrafficAnalayzer.Test
 {
     [TestClass]
-    public class TestSQLCEDataSource
+    public class TestSqlceDataSource
     {
 
-        private IDataSource dataSource;
+        private IDataSource _dataSource;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            dataSource = new SqlceDataSource();
+            _dataSource = new SqlceDataSource();
         }
 
         [TestMethod]
         public void TestGetVolume()
         {
+            //Get volume for intersection and detector that do exist
+            var volume = _dataSource.GetVolume(1, 2, DateTime.Now);
+
+        }
+
+        [TestMethod]
+        public void TestGetVolumeForTimePeriod()
+        {
+            var volumeForTimePeriod = _dataSource.GetVolumeForTimePeriod(1, new List<int>(), DateTime.Now, DateTime.Now);
+        }
+
+        [TestMethod]
+        public void TestGetTotalVolumeForDay()
+        {
+            var totalVolumeForDay = _dataSource.GetTotalVolumeForDay(DateTime.Today, 405, new List<int>());
+        }
+
+        [TestMethod]
+        public void TestGetIntersections()
+        {
+            var intersections = _dataSource.GetIntersections();
+            Assert.AreEqual(1, intersections.Count);
         }
     }
 }
