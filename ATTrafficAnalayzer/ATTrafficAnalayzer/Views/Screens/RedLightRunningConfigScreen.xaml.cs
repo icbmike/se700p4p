1db67a288a09580ed9f4d8ac4e7f871f24ea24c4
Mike Little <bio595@gmail.com>
2014-02-23 14:02:41 +1300

    Fixed an issue where report browser wouldnt update when things would delete

--- ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/RedLightRunningConfigScreen.xaml.cs
+++ ATTrafficAnalayzer/ATTrafficAnalayzer/Views/Screens/RedLightRunningConfigScreen.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ATTrafficAnalayzer.Annotations;
 using ATTrafficAnalayzer.Models;
-using ATTrafficAnalayzer.Models.ReportConfiguration;
 using ATTrafficAnalayzer.Modes;
 using MessageBox = System.Windows.Forms.MessageBox;
 
@@ -73,7 +63,8 @@ namespace ATTrafficAnalayzer.Views.Screens
                 reportConfigSelectedModel.Selected = false;
             }
             foreach (var reportConfigSelectedModel in Configuration.Sites.
-                SelectMany(reportConfiguration => ReportConfigurations.Where(reportConfigSelectedModel => reportConfigSelectedModel.Name == reportConfiguration.Name)))
+                SelectMany(reportConfiguration => ReportConfigurations.Where(reportConfigSelectedModel =>
+                    Equals(reportConfigSelectedModel.Name, reportConfiguration.Name))))
             {
                 reportConfigSelectedModel.Selected = true;
             }
